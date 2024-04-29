f7e002b69908ddfe7844e04dadc106a2895bfc4b
bastiyan <bastiyan@blymo.co..uk>
2024-04-29 15:28:27 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'HomeView',
+});
+
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>Welcome to the Home page</p>
+  </main>
+</template>
